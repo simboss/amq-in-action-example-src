9b5bab32fe0b5820159f0280482d00900854f80a
joseph <joseph.siyi@gmail.com>
2013-06-28 15:14:49 +0800

    use windows server

--- src/main/java/org/apache/activemq/book/ch7/sync/Client.java
+++ src/main/java/org/apache/activemq/book/ch7/sync/Client.java
@@ -17,7 +17,7 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 public class Client implements MessageListener {
 
-	private String brokerUrl = "tcp://121.199.0.190:61616";
+	private String brokerUrl = "tcp://42.121.0.245:61616";
 	private String requestQueue = "requests";
 	
 	Connection connection;
